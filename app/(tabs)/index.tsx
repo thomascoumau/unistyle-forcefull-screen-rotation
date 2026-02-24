9770f8e527fb1823ddaea5276f87bde3fefc6648
Thomas Coumau <thomasco@bam.tech>
2026-02-24 11:27:09 +0100

    feat: 🎸 add rotation lock

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,46 @@
 /** @format */
 
+import "@/style/unistyle";
+import {
+  lockAsync,
+  OrientationLock,
+  unlockAsync,
+} from "expo-screen-orientation";
 import { StyleSheet } from "react-native-unistyles";
 
 import { HelloWave } from "@/components/hello-wave";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
+import { Pressable } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <ThemedView style={styles.titleContainer}>
-      <ThemedText style={styles.title} type="title">
-        Welcome!
-      </ThemedText>
-      <HelloWave />
+    <ThemedView style={styles.container}>
+      <Pressable onPress={() => void unlockAsync()}>
+        <ThemedText>Unlock</ThemedText>
+      </Pressable>
+      <Pressable onPress={() => void lockAsync(OrientationLock.PORTRAIT_UP)}>
+        <ThemedText>Lock portrait</ThemedText>
+      </Pressable>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText style={styles.title} type="title">
+          Welcome!
+        </ThemedText>
+        <HelloWave />
+      </ThemedView>
     </ThemedView>
   );
 }
 
-const styles = StyleSheet.create((_, { screen }) => ({
-  titleContainer: {
+const styles = StyleSheet.create((_, runtime) => ({
+  container: {
+    paddingTop: runtime.insets.top,
+    gap: 30,
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleContainer: {
     justifyContent: "center",
     flexDirection: "row",
     alignItems: "center",
@@ -27,6 +48,10 @@ const styles = StyleSheet.create((_, { screen }) => ({
   },
   title: {
     backgroundColor: "blue",
-    width: screen.width * 0.5,
+    width: {
+      sm: "50%",
+      md: "80%",
+      lg: 500,
+    },
   },
 }));
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,45 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+/** @format */
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import "@/style/unistyle";
+
+import { OrientationLock, lockAsync } from "expo-screen-orientation";
+import { useEffect } from "react";
+
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  useForcePortraitOrientation();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: "modal", title: "Modal" }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
+
+const useForcePortraitOrientation = () => {
+  useEffect(() => {
+    void lockAsync(OrientationLock.PORTRAIT_UP);
+  }, []);
+};
