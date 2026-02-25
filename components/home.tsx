aa71e33d51717bedbdf049497c6e8cb1527f2abb
Thomas Coumau <thomasco@bam.tech>
2026-02-25 10:07:32 +0100

    feat: 🎸 remove unecessary code for reproduction

--- components/home.tsx
+++ components/home.tsx
@@ -1,13 +1,8 @@
 /** @format */
 
-import {
-  lockAsync,
-  OrientationLock,
-  unlockAsync,
-} from "expo-screen-orientation";
+import { unlockAsync } from "expo-screen-orientation";
 import { Pressable } from "react-native";
 import { StyleSheet } from "react-native-unistyles";
-import { HelloWave } from "./hello-wave";
 import { ThemedText } from "./themed-text";
 import { ThemedView } from "./themed-view";
 
@@ -17,15 +12,9 @@ export const Home = () => {
       <Pressable onPress={() => void unlockAsync()}>
         <ThemedText>Unlock</ThemedText>
       </Pressable>
-      <Pressable onPress={() => void lockAsync(OrientationLock.PORTRAIT_UP)}>
-        <ThemedText>Lock portrait</ThemedText>
-      </Pressable>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText style={styles.title} type="title">
-          Welcome!
-        </ThemedText>
-        <HelloWave />
-      </ThemedView>
+      <ThemedText style={styles.title} type="title">
+        Welcome!
+      </ThemedText>
     </ThemedView>
   );
 };
@@ -37,22 +26,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  titleContainer: {
-    justifyContent: "center",
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
   title: {
     backgroundColor: "blue",
     width: {
       sm: "50%",
       md: "80%",
-      lg: 500,
-    },
-    alignItems: {
-      sm: "center",
-      md: "flex-start",
     },
   },
 });
